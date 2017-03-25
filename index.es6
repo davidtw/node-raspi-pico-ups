4ef49cd040b1e066431142f475634977246e5ec6
David Twigger <davidtwigger@gmail.com>
2017-03-25 15:09:17 +0100

    fix syntaxe

--- index.es6
+++ index.es6
@@ -2,9 +2,15 @@
     const pico = require('./src/pico');
 
     pico.getCurrentPowerMode()
-        .then((val) => console.log('Current power mode :', pico.powerModes[val]));
-    pico.getCurrentBatteryVoltage()
-        .then((val) => console.log('Current Power voltage :', val));
-    pico.getCurrentRpiVoltage()
+        .then((val) => {
+            console.log('Current power mode :', pico.powerModes[val]);
+            return pico;
+        })
+        .getCurrentBatteryVoltage()
+        .then((val) => {
+            console.log('Current Power voltage :', val);
+            return pico;
+        })
+        .getCurrentRpiVoltage()
         .then((val) => console.log('Current Power voltage :', val));
 }());
--- index.js
+++ index.js
@@ -4,12 +4,12 @@
     var pico = require('./src/pico');
 
     pico.getCurrentPowerMode().then(function (val) {
-        return console.log('Current power mode :', pico.powerModes[val]);
-    });
-    pico.getCurrentBatteryVoltage().then(function (val) {
-        return console.log('Current Power voltage :', val);
-    });
-    pico.getCurrentRpiVoltage().then(function (val) {
+        console.log('Current power mode :', pico.powerModes[val]);
+        return pico;
+    }).getCurrentBatteryVoltage().then(function (val) {
+        console.log('Current Power voltage :', val);
+        return pico;
+    }).getCurrentRpiVoltage().then(function (val) {
         return console.log('Current Power voltage :', val);
     });
 })();
